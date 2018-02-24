a8561529283b113c448c2e7a2d0c3acb7cf6de94
Francis <song.chaoke@immomo.com>
2018-02-24 12:05:38 +0800

    build my packagist

---
+++ src/Ford/Focus/Focus2018.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ford\Focus;
+class Focus2018
+{
+    public function info()
+    {
+        echo "This is Ford Focus2018!<br>";
+    }
+}
---
+++ src/Ford/Fusion/Fusion2018.php
@@ -0,0 +1,9 @@
+<?php
+namespace Ford\Fusion;
+class Fusion2018
+{
+    public function info()
+    {
+        echo 'This is Ford Fusion2018 !<br>';
+    }
+}
